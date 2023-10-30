939a3476d0f37226783f92b36dec3b56420835a3
jochoa <jonatan.c.ochoa@gmail.com>
2023-10-30 12:39:04 -0300

    config Routes and new App login

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import './App.css'
-import Dashboard from './components/dashboard/Dashboard';
 import { StickyFooter } from './components/dashboard/StickyFooter';
 import { AppRoutes } from './routes/AppRoutes';
-import { BrowserRouter, Link } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
 
   return (
     <BrowserRouter>
-    <Dashboard />
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/register">Register</Link></li>
-          <li><Link to="/katas">Katas</Link></li>
-        </ul>
-      </nav>
       <AppRoutes />
       <StickyFooter />
     </BrowserRouter>
